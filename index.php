c27e55865628481de89d06457fdeb8f445416ca3
Miloudy bouchra <miloudybouchra01@gmail.com>
2024-12-31 21:52:46 +0100

    organisation project

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Document</title>
 </head>
-<body>
-    <h1>meriem</h1>
+<body >
+    <h1>test tailwind</h1>
 </body>
 </html>
