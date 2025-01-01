c96776e197a36e368abd8007575e1436c5ea3e62
Miloudy bouchra <miloudybouchra01@gmail.com>
2025-01-01 13:08:04 +0100

    header and footer

--- index.php
+++ index.php
@@ -6,7 +6,20 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Document</title>
 </head>
-<body >
-    <h1>test tailwind</h1>
+<body class="bg-[#f9dbbd]">
+    <!-- header  header  header  header  header  header  header  header  header  header  header  header  header  header  header  header  -->
+    <?php include "header.php" ; ?>
+    <!-- header  header  header  header  header  header  header  header  header  header  header  header  header  header  header  header  -->
+
+
+
+    <h1>mon code ici !!!</h1>
+
+    <!-- footer  footer  footer  footer  footer  footer  footer  footer  footer  footer  footer  footer  footer  footer  footer  footer -->
+     <?php include "footer.php" ; ?>
+    <!-- footer  footer  footer  footer  footer  footer  footer  footer  footer  footer  footer  footer  footer  footer  footer  footer -->
 </body>
+
+<script src="js/header.js"></script>
+
 </html>
